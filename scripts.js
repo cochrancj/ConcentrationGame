33836022b04d9752449b59ed5733c282f6c5c95c
Carolyn Cochran <cochran.cj@gmail.com>
2016-03-25 17:07:43 -0400

    adds some psuedo code

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
 console.log("...loaded");
+
+Store tiles (50 total - 25 of each) in two arrays.
+
+use math.random to randomize tiles in each array.
+
+populate board with random tiles (face down).
+
+Show all cards briefly (toggle class face up with a timeout of less than a second).
+
+Flip tiles back over.
+
+When the player clicks a tile, flip tile over; do a second time.
+
+If tiles match, remove from board; if they do not match, flip both back over.
+
+Game setup: allow player to choose from 3 different card styles - slightly different RGBs, hipster owls or 
