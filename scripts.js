e676a60816482ebc4905f7e68db9ab4e453211de
eekej001 <eekej001@gmail.com>
2016-04-11 05:59:43 -0700

    Update scripts.js
    Most of the change have been noted within the document.  Some of the major changes include:
    -Added winner class when the game is won.
    -Added button to play again when game is won.
    -Prevented user from breaking the game by rapidly clicking on cards.
    -Added wiggle function.
    -Added marvel deck.
    -Added feature that allows user to choose their deck at the beginning of the game.
    -Game Score div updates text depending on the deck that the user chooses.
    -Deck icon that is not chosen is greyed out.
    -Minor animation added when deck is chosen.
    -Added clock to the game.
    -Stopped clicks from being incremented after game completion.
    
    I'm not sure what you were trying to do with the countdown clock and the cheater function, so I left that code alone.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,220 @@
 console.log("...loaded");
 
-var makeBoard;
-
 $(document).ready(function() {
+	
+	//THIS ACTIVATES MARVEL DECK ON CLICK AND DISABLES THE ABILITY TO ACTIVATE ANOTHER DECK ONCE THE GAME STARTS
+	$('.pick-deck1').click(function () {
+		$('.pick-deck1').unbind('click');
+		$('.pick-deck2').unbind('click');
+		$('.pick-deck1').animate({height: "105px"}).animate({height: "100px"});
+		$('.pick-deck2').addClass('deckChosen');
+		$('h3').first().html('Marvel Deck');
+		$('h3').first().append('<h4>Make Some Matches!</h4>');
+			
+					
+var marvel = [
+    {src: 'images/marvel-01.png', data: 01},
+    {src: 'images/marvel-02.png', data: 02},
+    {src: 'images/marvel-03.png', data: 03},
+    {src: 'images/marvel-04.png', data: 04},
+    {src: 'images/marvel-05.png', data: 05},
+    {src: 'images/marvel-06.png', data: 06},
+    {src: 'images/marvel-07.png', data: 07},
+    {src: 'images/marvel-08.png', data: 08},
+    {src: 'images/marvel-09.png', data: 09},
+    {src: 'images/marvel-010.png', data: 10},
+    {src: 'images/marvel-01.png', data: 01},
+    {src: 'images/marvel-02.png', data: 02},
+    {src: 'images/marvel-03.png', data: 03},
+    {src: 'images/marvel-04.png', data: 04},
+    {src: 'images/marvel-05.png', data: 05},
+    {src: 'images/marvel-06.png', data: 06},
+    {src: 'images/marvel-07.png', data: 07},
+    {src: 'images/marvel-08.png', data: 08},
+    {src: 'images/marvel-09.png', data: 09},
+    {src: 'images/marvel-010.png', data: 10}
+   ]
+
+
+//THIS MAKES A DIV FOR EACH CARD AND POPULATES WITH AN IMAGE FROM THE ARRAY. IT ALSO SETS A TIMEOUT ON THE LOAD FLASH AND SETS ALL IMAGES TO FACEDOWN TO CONTINUE
+makeBoard =  function(){
+    for (var i = 0; i < marvel.length; i++) {
+      $('#game-board').append($('<div>').addClass('card ' + 'temp-faceup ').attr('data',marvel[i].data).css('background-image', 'url(' + marvel[i].src + ')'));
+	};
+
+// When the board is ready and the page loads, show all cards briefly on load (toggle class face up with a timeout of less than a second) so player can preview matches.
+// Flip tiles back over to begin - use a nifty animation.
+    window.setTimeout(function() {
+	  $('.card').toggleClass('temp-faceup');
+      //$('.card').addClass('facedown');  // Unnecessary facedown class addition
+      $('.card').css('background-image','url(images/card-facedown-2.jpg)');
+    },2000);
+  };  
+
+		
+
+// FISHER YATES SHUFFLE METHOD - THIS SHUFFLES THE 'CARDS'
+function shuffle () {
+  for (i = marvel.length - 1; i > 0; i -= 1) {
+    var j = Math.floor(Math.random() * (i + 1))
+    var temp = marvel[i]
+    marvel[i] = marvel[j]
+    marvel[j] = temp
+  }
+  console.log(marvel)
+};
+
+shuffle();
+makeBoard();
+
+
+
+// THIS ADDS THE BLINK
+function initBoard() {
+
+  $('.card').delay(100).fadeOut('slow').fadeIn('slow').toggleClass('facedown'); 
+};
+
+initBoard();
+
+// When the player clicks a tile, flip tile over with an animation and toggle the class to face up; do a second time with your second click.
+
+
+//THIS LETS THE USER KNOW IF THEY'VE WON AND RELOADS SESSION IF THEY WANT TO PLAY AGAIN
+var permFace = 0;
+var winConfirm = 0;
+	setInterval(function(){
+	if (permFace === 10 && sec > 0 && winConfirm === 0) { 
+	    $('body').addClass('winner');
+		$('h1').append('<h2>You Win!</h2>');
+		$('h2').append('<div id="playAgain">Play Again?</div>');
+		$('#playAgain').click(function () {
+			location.reload();
+		});
+		winConfirm++;
+			clearInterval(gameTimer);
+		} }, 700);
+
+//THIS REMOVES THE WIGGLE CLASSES THAT ARE ADDED IF THE USER RAPIDLY CLICKS CARDS	
+setInterval(function() {
+	var decSpam = $('.temp-faceup');
+	var decWigL = $('.wiggle').length;
+	if (clicks%2 == 1 && decSpam.length == 0) {
+		clicks--;
+	} }, 1);	
+	
+
+	
+var clicks = 0;
+
+	
+$('.facedown').click(function() {
+  var $currentCard = $(this);
+  var $currentData = $(this).attr('data');
+
+  if (($(this).hasClass('faceup') == false) && ($(this).hasClass('temp-faceup') == false)){
+  clicks++;
+	}	
+var tempClicks = clicks;	
+  
+  $(this).addClass('stopFlipping');
+  var cardCheck = $('.card');  
+  
+ 
+  
+  //THIS ADDS THE WIGGLE CLASS TO THE MATCH OF THE CARD THAT IS CLICKED
+  var hintOn = setTimeout (function() {
+		  for (i = 0; i < cardCheck.length; i++) {
+			  if ((marvel[i].data == $currentData) && ($(cardCheck[i]).hasClass('facedown') == true) && ($currentCard.hasClass('temp-faceup') == true) && ($flippedCards.length === 1) && (clicks%2 != 0) && ($(cardCheck[i]).hasClass('stopFlipping') == false) && (clicks == tempClicks)){
+				  $(cardCheck[i]).addClass('wiggle');  			  
+		  }
+		  }
+  }, 5000);
+  
+  $(this).removeClass('stopFlipping');
+
+ 
+//ADDED CONDITIONS TO STOP THE USER FROM RAPIDLY CLICKING CARDS AND BREAKING THE GAME
+ 
+ if (($('.temp-faceup').length < 2) && ($(this).hasClass('faceup') == false) && ($(this).hasClass('temp-faceup') == false)) {
+  $currentCard.toggleClass("facedown");
+  $currentCard.toggleClass("temp-faceup");
+  $currentCard.css('background-image','url( images/marvel-0'+$currentCard.attr('data')+'.png)');
+	}
+	
+	
+  // console.log('click!');
+
+  // COMPARE CARDS ON CLICK OF CARD
+  // If tiles match, keep perisistently up; if they do not match, flip both cards back over (face down class).
+  //
+
+  var $flippedCards = $('.temp-faceup');
+
+  if ($flippedCards.length === 2) {
+    // console.log("match");
+	//
+    var marvelData1 = $flippedCards.eq(0).attr('data');
+    var marvelData2 = $flippedCards.eq(1).attr('data');
+    if (marvelData1 === marvelData2) {
+      $flippedCards.addClass('faceup').removeClass('temp-faceup');   //.off();
+	  permFace++;
+	  
+    } else {
+      setTimeout(function () {
+      $flippedCards.addClass('facedown').removeClass('temp-faceup').css('background-image','url(images/card-facedown-2.jpg)');
+    }, 700);//THIS IS NOT WORKING 
+    }
+  }
+
+});
+
+
+
+$('.card').click(function() {
+	if (winConfirm === 0) {
+    $('#click-count').html(function(i, val) { return val*1+1 });
+		}
+});
+
+//THIS ADDS A TIMER TO THE GAME
+	var sec = 0;
+		var gameTimer = setInterval( function(){
+			$("#seconds").html(pad(++sec%60));
+			$("#minutes").html(pad2(parseInt(sec/60, 10)));
+		}, 1000);
+
+		function pad (timePassed) {
+		if (timePassed.toString().length < 2) {
+			return ("0" + timePassed);
+			}
+			else {
+			return timePassed;
+				}
+		}  
+		
+		function pad2 (timePassed2) {
+		if (timePassed2.toString().length < 2) {
+			return ("0" + timePassed2 + ":");
+			}
+			else {
+			return (timePassed2 + ":");
+				}
+		}  
+	
+	});
+
+
+// OWL DECK CODE ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++	
+
+	
+	$('.pick-deck2').click(function () {
+		$('.pick-deck1').unbind('click');
+		$('.pick-deck2').unbind('click');
+		$('.pick-deck1').addClass('deckChosen');
+		$('h3').first().html('Owl Deck');
+		$('h3').first().append('<h4>Make Some Matches!</h4>');
+
 
   var owls = [
     {src: 'images/owls-01.png', data: 01},
@@ -27,20 +239,23 @@ $(document).ready(function() {
     {src: 'images/owls-010.png', data: 10}
    ]
 
+
 //THIS MAKES A DIV FOR EACH CARD AND POPULATES WITH AN IMAGE FROM THE ARRAY. IT ALSO SETS A TIMEOUT ON THE LOAD FLASH AND SETS ALL IMAGES TO FACEDOWN TO CONTINUE
 makeBoard =  function(){
     for (var i = 0; i < owls.length; i++) {
       $('#game-board').append($('<div>').addClass('card ' + 'temp-faceup ').attr('data',owls[i].data).css('background-image', 'url(' + owls[i].src + ')'));
-    };
+	};
 
 // When the board is ready and the page loads, show all cards briefly on load (toggle class face up with a timeout of less than a second) so player can preview matches.
 // Flip tiles back over to begin - use a nifty animation.
     window.setTimeout(function() {
-      $('.card').toggleClass('temp-faceup');
-      $('.card').addClass('facedown');
+	  $('.card').toggleClass('temp-faceup');
+      //$('.card').addClass('facedown');  // Unnecessary facedown class addition
       $('.card').css('background-image','url(images/card-facedown-2.jpg)');
     },2000);
-  };
+  };  
+
+		
 
 // FISHER YATES SHUFFLE METHOD - THIS SHUFFLES THE 'CARDS'
 function shuffle () {
@@ -57,25 +272,76 @@ shuffle();
 makeBoard();
 
 
+
 // THIS ADDS THE BLINK
 function initBoard() {
 
-  $('.card').delay(100).fadeOut('slow').fadeIn('slow').toggleClass('facedown');
+  $('.card').delay(100).fadeOut('slow').fadeIn('slow').toggleClass('facedown'); 
 };
 
 initBoard();
 
-
 // When the player clicks a tile, flip tile over with an animation and toggle the class to face up; do a second time with your second click.
 
 
+
+var permFace = 0;
+var winConfirm = 0;
+	setInterval(function(){
+	if (permFace === 10 && sec > 0 && winConfirm === 0) { 
+	    $('body').addClass('winner');
+		$('h1').append('<h2>You Win!</h2>');
+		$('h2').append('<div id="playAgain">Play Again?</div>');
+		$('#playAgain').click(function () {
+			location.reload();
+		});
+		winConfirm++;
+			clearInterval(gameTimer);
+		} }, 700);
+
+	
+setInterval(function() {
+	var decSpam = $('.temp-faceup');
+	var decWigL = $('.wiggle').length;
+	if (clicks%2 == 1 && decSpam.length == 0) {
+		clicks--;
+	} }, 1);	
+	
+
+	
+var clicks = 0;
+
+	
 $('.facedown').click(function() {
   var $currentCard = $(this);
+  var $currentData = $(this).attr('data');
+
+  if (($(this).hasClass('faceup') == false) && ($(this).hasClass('temp-faceup') == false)){
+  clicks++;
+}	
+var tempClicks = clicks;
+  
+  $(this).addClass('stopFlipping');
+  var cardCheck = $('.card');  
+
+  
+  var hintOn = setTimeout (function() {
+		  var faceDcheck = $('.facedown'); 
+		  for (i = 0; i < cardCheck.length; i++) {
+			  if ((owls[i].data == $currentData) && ($(cardCheck[i]).hasClass('facedown') == true) && ($currentCard.hasClass('temp-faceup') == true) && ($flippedCards.length === 1) && (clicks%2 != 0) && ($(cardCheck[i]).hasClass('stopFlipping') == false) && (clicks == tempClicks)){
+				  $(cardCheck[i]).addClass('wiggle');  
+					
+		  }
+		  }
+  }, 5000);
+  
+  $(this).removeClass('stopFlipping');
+  
+ if (($('.temp-faceup').length < 2) && ($(this).hasClass('faceup') == false) && ($(this).hasClass('temp-faceup') == false)) {
   $currentCard.toggleClass("facedown");
   $currentCard.toggleClass("temp-faceup");
   $currentCard.css('background-image','url( images/owls-0'+$currentCard.attr('data')+'.png)');
-
-
+	}
   // console.log('click!');
 
   // COMPARE CARDS ON CLICK OF CARD
@@ -87,31 +353,56 @@ $('.facedown').click(function() {
 
   if ($flippedCards.length === 2) {
     // console.log("match");
+	//
     var owlData1 = $flippedCards.eq(0).attr('data');
     var owlData2 = $flippedCards.eq(1).attr('data');
-
     if (owlData1 === owlData2) {
-      $flippedCards.addClass('faceup').removeClass('temp-faceup').off();
+      $flippedCards.addClass('faceup').removeClass('temp-faceup');   //.off();
+	  permFace++;
+	  
     } else {
       setTimeout(function () {
       $flippedCards.addClass('facedown').removeClass('temp-faceup').css('background-image','url(images/card-facedown-2.jpg)');
-    }, 700);//THIS IS NOT WORKING
+    }, 700);//THIS IS NOT WORKING 
     }
   }
-
-
-
-
 
 });
 
 
 
 $('.card').click(function() {
-
+	if (winConfirm === 0) {
     $('#click-count').html(function(i, val) { return val*1+1 });
+		}
 });
 
+	var sec = 0;
+		var gameTimer = setInterval( function(){
+			$("#seconds").html(pad(++sec%60));
+			$("#minutes").html(pad2(parseInt(sec/60, 10)));
+		}, 1000);
+
+		function pad (timePassed) {
+		if (timePassed.toString().length < 2) {
+			return ("0" + timePassed);
+			}
+			else {
+			return timePassed;
+				}
+		}  
+		
+		function pad2 (timePassed2) {
+		if (timePassed2.toString().length < 2) {
+			return ("0" + timePassed2 + ":");
+			}
+			else {
+			return (timePassed2 + ":");
+				}
+		}  
+	
+		
+	});	
 });
 
 //NO CONSOLE ERRORS; DOES NOT SHOW UP
@@ -151,7 +442,7 @@ function youWin() {
   if (($('.temp-faceup') === 0) && ($('.facedown') === 0)) {
     $('body').addClass('winner');
   }
-};
+}; 
 
 function winnerCheat() {
   var $cheat = $('#cheat');
@@ -166,6 +457,9 @@ function winnerCheat() {
     }
   }, 60);
 };
+
+
+
 
 // If player takes too long in between choices, wiggle matches or make glow.
 
@@ -207,6 +501,8 @@ function winnerCheat() {
 
       // (create versions of this for the three card type options I want to build - marvel heroes + villains, owls & slightly different RGB values - if these images don't work, use 8-bit food icons + rgb values + geometrical shapes or Pokemon or tarot cards) **- V2 -**
 
+	  
+/*	  
   $('.pick-deck1').click(function() {
     $(this).toggleClass('pick-marvel');
   });
@@ -217,5 +513,5 @@ function winnerCheat() {
 
   $('.pick-deck3').click(function() {
     $(this).toggleClass('pick-rgb');
-  });
+  }); */
 // ++++++++++++++++++++++++++++
